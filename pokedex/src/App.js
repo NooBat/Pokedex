731df08e45704c2845ec58c47ed072bb901718ae
NooBat <khoi27012003@gmail.com>
2022-06-28 17:51:37 +0700

    commit by vscode

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,6 +1,19 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const App = () => {
-  const a = 1;
-  return a;
+  const [pokedex, setPokedex] = useState([]);
+  const [searchBar, setSearchBar] = useState('');
+  
+  useEffect(() => {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon/?limit=1154')
+      .then((response))
+  }, [])
+
+  return (
+    <div></div>
+  );
 };
 
 export default App;
