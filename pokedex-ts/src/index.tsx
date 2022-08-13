60605eb6ef1a7cccd721381803c5749f03c01580
NooBat <khoi27012003@gmail.com>
2022-08-14 03:12:44 +0700

    added prettier as a devDeps

--- pokedex-ts/src/index.tsx
+++ pokedex-ts/src/index.tsx
@@ -4,9 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Router>
     <App />
